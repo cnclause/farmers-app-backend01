660fc67e8916de9d0923f38afe2be6bf32f2fad8
github username <github email address>
2019-12-10 16:23:50 -0700

    changed knexfile.js to add production trying to deploy 2

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection:  'postgres://localhost/farmers-db'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
