9aa63a50218c33ff6534dc803104339dc400bebf
github username <github email address>
2019-12-10 17:13:32 -0700

    updated cors pollicy header in app.js because was receiving cors error with deployment

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(passport.initialize())
 app.use(checkAuthHeaderSetUser)
 
